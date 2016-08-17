c1ae46f7719cc8db78163d33571c7063c936fe41
8adam95 <adam.szefer@gmail.com>
2016-08-17 13:43:02 +0200

    adding existing projects to the git

---
+++ Udacity/app/src/main/java/com/example/adam/udacity/Udacity.java
@@ -0,0 +1,13 @@
+package com.example.adam.udacity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Udacity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_udacity);
+    }
+}
