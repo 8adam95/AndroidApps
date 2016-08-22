cd793796a4c7370ced67f8c05d9e3a1e65407236
8adam95 <adam.szefer@gmail.com>
2016-08-22 22:24:36 +0200

    50% of a course

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,6 +9,8 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +18,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        int quantity = 5;
-        display(quantity);
-        displayPrice(quantity*5);
+
+        displayPrice(quantity * 5);
     }
+
+    public void increment(View view)
+    {
+        quantity = quantity+1;
+        display(quantity);
+    }
+
+    public void decrement(View view)
+    {
+        quantity = quantity-1;
+        display(quantity);
+    }
+
 
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
